4b0bcaa3ee6c5a551026dd5b357e78581a8a6d7e
krashen <pablo.ferreyra.cxs@gmail.com>
2023-12-17 19:46:32 -0300

    set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 
+import './categories.styles.scss';
+
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Shoes',
+    },
+    {
+      id: 3,
+      title: 'Ties',
+    },
+    {
+      id: 4,
+      title: 'Damian',
+    },
+    {
+      id: 5,
+      title: 'Corchos',
+    },
+  ]
   return (
-    <div>
-      Hello World
+    <div className='categories-container'>
+      {categories.map(({title, id})=>(
+          <div key={id} className='category-container'>
+            <div className='background-image' />
+            <div className='category-body-container'>
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+      ))}          
     </div>
   );
 }
