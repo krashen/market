4232b20b6802e82fa75832e2e9f1f4ce4e8f7f3a
krashen <pablo.ferreyra.cxs@gmail.com>
2024-01-23 22:00:31 -0300

    first commit

---
+++ src/utils/reducer/reducer.utils.ts
@@ -0,0 +1,12 @@
+import { UnknownAction } from 'redux';
+
+export type ActionWithPayload<T, P> = {
+    type: T,
+    payload: P,
+}
+
+export type Action<T> = {
+    type: T;
+}
+
+export const createAction = (type, payload) => ({ type, payload })
